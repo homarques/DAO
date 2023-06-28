a175b46891df554dc79b49e817cac0575416b455
Henrique Oliveira Marques <oli@sdu.dk>
2023-06-28 11:22:44 +0200

    Update plots.r

--- R/plots.r
+++ R/plots.r
@@ -1,6 +1,7 @@
 if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
 if (!require('scmamp')) install.packages('scmamp'); library('scmamp')
 if (!require('RColorBrewer')) install.packages('RColorBrewer'); library('RColorBrewer')
+options(scipen=999)
 
 source('R/latex.r')
 
@@ -24,12 +25,14 @@ plotSynth <- function () {
 	df <- data.frame(Algorithms=rep(names, each = 16), Dimensionality=rep(seq(2, 32, 2), 8), SD = c(results_sd), AUCROC = c(results_mean))
 	df$Algorithms = factor(df$Algorithms, levels = c("DAO (MLE)", "DAO (TLE)", "DAO (TwoNN)", "LOF", "SLOF", "DAO (LIDL MoG)", "DAO (LIDL RQSNF)", "KNN"))
 
-	pdf("synthetic.pdf", width=28, height = 18)
-	ggplot(data=df, aes(x=Dimensionality, y=AUCROC, group=Algorithms, color=Algorithms, shape = Algorithms)) + geom_line() + geom_point(size = 8) + geom_segment(aes(x = 8, y = -Inf, xend = 8, yend = 1), linetype='C9',colour='black' ) +
+	p = ggplot(data=df, aes(x=Dimensionality, y=AUCROC, group=Algorithms, color=Algorithms, shape = Algorithms)) + geom_line() + geom_point(size = 8) + geom_segment(aes(x = 8, y = -Inf, xend = 8, yend = 1), linetype='C9',colour='black' ) +
 	scale_color_manual(values = c("#B2DF8A", "#33A02C",  "#FB9A99", "#FDBF6F","#FF7F00", "#A6CEE3", "#1F78B4", "#E31A1C"), labels = c(bquote(DAO[MLE]), bquote(DAO[TLE]), bquote(DAO[TwoNN]), "LOF", "SLOF", bquote(DAO[LIDL(MoG)]), bquote(DAO[LIDL(RQ-NSF)]), "KNN")) +
 	scale_shape_manual(values = c(15, 3, 7, 0 , 6,16, 17,8), labels = c(bquote(DAO[MLE]), bquote(DAO[TLE]), bquote(DAO[TwoNN]), "LOF", "SLOF", bquote(DAO[LIDL(MoG)]), bquote(DAO[LIDL(RQ-NSF)]), "KNN")) +
 	theme_classic() + theme(plot.margin = unit(c(-0.1,0,-0.25,0.1), "cm"), axis.ticks.length=unit(.45, "cm"), axis.title.y = element_text(margin = margin(t = 0, r = 5, b = 0, l = 0)), axis.text.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)), axis.text.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)), axis.title.x = element_text(margin = margin(t = 30, r = 0, b = 0, l = 0)), legend.justification = "top", panel.grid.major = element_line(), axis.text=element_text(size=38), legend.key.size=unit(4, "line"), axis.title=element_text(size=56), legend.text=element_text(size=46), legend.title=element_text(size=50), panel.grid.minor = element_line() ) + 
 	ylab("ROC AUC") + xlab(expression(paste("Intrinsic dimension of ", italic("c"[2])))) + scale_x_discrete(limits=c(2, 4, 8, 16, 24, 32)) + geom_errorbar(aes(ymin=AUCROC-SD, ymax=AUCROC+SD), width=0.3)
+	
+	ggsave("synthetic.png", width=28, height = 18)
+	print(p)
 	dev.off()
 
 }
@@ -108,14 +111,14 @@ summaryResultsSyntheticDatasets <- function () {
 	results_mean = c()
 	results_sd = c()
 	overall_mean = colMeans(results_table)
-	overall_mean = round(overall_mean, 3)
+	overall_mean = round(overall_mean, 4)
 	overall_sd = apply(results_table, 2, sd)
 	overall_sd = round(overall_sd, 4)
 	for(i in 1:16){
 		results_mean = rbind(results_mean, colMeans(results_table[seq(i, 16*24, 16),]))
 		results_sd = rbind(results_sd, apply(results_table[seq(i, 16*24, 16),], 2, sd))
 	}
-	results_mean = round(results_mean, 3)
+	results_mean = round(results_mean, 4)
 	results_sd = round(results_sd, 4)
 
 	results_table = matrix(0, ncol = 8, nrow = 17)
@@ -128,13 +131,13 @@ summaryResultsSyntheticDatasets <- function () {
 	results_table[i, which(max(overall_mean)==overall_mean)] = bold(paste(overall_mean[which(max(overall_mean) == overall_mean)], ' $\\pm$ ', overall_sd[which(max(overall_mean) == overall_mean)]))
 	results_table[i, which(max(overall_mean)!=overall_mean)] = paste(overall_mean[which(max(overall_mean) != overall_mean)], ' $\\pm$ ', overall_sd[which(max(overall_mean) != overall_mean)])
 
-	results_table = cbind(c(paste('Dimensionality', seq(2,32,2)), 'Overall'), results_table)
+	results_table = cbind(c(paste('Intrinsic Dimension = ', seq(2,32,2)), 'Overall'), results_table)
 	colnames(results_table) = names
 	xtable(results_table, caption("ROC AUC values of the methods on real datasets", label = "tab:res:synth:datasets"), alignment = "lcccccccc")
 }
 
 
-plotRuntime <- function () {
+printRuntime <- function () {
 	dao = c()
 	lof = c()
 	knno = c()
@@ -178,7 +181,7 @@ plotCDRealDatasets <- function () {
 }
 
 
-plotLRSynthetic <- function () {
+lrSyntheticDatasets <- function () {
 	results = list.files('experiments/synthetic', full.names = T)
 	names = c("KNN", "LOF", "SLOF", "DAO (LIDL RQSNF)", "DAO (LIDL MoG)", "DAO", "DAO (TLE)", "DAO (TwoNN)")
 	results_table = c()
@@ -192,6 +195,17 @@ plotLRSynthetic <- function () {
 		results_mean = rbind(results_mean, colMeans(results_table[seq(i, 16*24, 16),]))
 	}
 	colnames(results_mean) = names
+
+	diffscore = results_mean[, 'DAO'] - results_mean[,'KNN']
+	tb = as.data.frame(cbind(diffscore, abs(seq(2, 32, 2) - 8)))
+	colnames(tb) = c('diff', 'diffIDs')
+
+	lr = lm(tb$diff ~  tb$diffIDs)
+	f = summary(lr)$fstatistic
+	pvalue = pf(f[1], f[2], f[3], lower.tail = F)
+
+	cat("$\\DAO$ - $\\KNN$   & ",  round(lr$coefficients[2], 4), " & ", formatC(pvalue, format = "e"), " & ", round(cor(tb$diff, tb$diffIDs), 3), "\\\\\n")
+
 
 	diffscore = results_mean[, 'DAO'] - results_mean[,'SLOF']
 	tb = as.data.frame(cbind(diffscore, abs(seq(2, 32, 2) - 8)))
@@ -213,20 +227,10 @@ plotLRSynthetic <- function () {
 
 	cat("$\\DAO$ - $\\LOF$   & ",  round(lr$coefficients[2], 4), " & ", formatC(pvalue, format = "e"), " & ", round(cor(tb$diff, tb$diffIDs), 3), "\\\\\n")
 
-	diffscore = results_mean[, 'DAO'] - results_mean[,'KNN']
-	tb = as.data.frame(cbind(diffscore, abs(seq(2, 32, 2) - 8)))
-	colnames(tb) = c('diff', 'diffIDs')
-
-	lr = lm(tb$diff ~  tb$diffIDs)
-	f = summary(lr)$fstatistic
-	pvalue = pf(f[1], f[2], f[3], lower.tail = F)
-
-	cat("$\\DAO$ - $\\KNN$   & ",  round(lr$coefficients[2], 4), " & ", formatC(pvalue, format = "e"), " & ", round(cor(tb$diff, tb$diffIDs), 3), "\\\\\n")
-
 }
 
 
-plotLRReal <- function () {
+lrRealDatasets <- function () {
 	results = list.files('experiments/stats', full.names = F)
 	overall = c()
 	stats = c()
@@ -287,13 +291,14 @@ plotLRReal <- function () {
 }
 
 
-plotRMoransI <- function () {
+plot_R_MoransI <- function () {
 	results = list.files('experiments/stats', full.names = F)
 	overall = c()
 	stats = c()
 	for(result in results){
 		data = read.table(paste0('experiments/real/', result), sep = ',')
 		stat = read.table(paste0('experiments/stats/', result), sep = ',')
+		stat[1]=2*stat[1]
 
 		overall = rbind(overall, data)
 		stats = rbind(stats, stat)
@@ -304,7 +309,6 @@ plotRMoransI <- function () {
 	R_title <- expression("R")
 	limit = c(-0.2, 0.2)
 
-	pdf("slof.pdf", width=9)
 	diffscore = overall[, 'DAO'] - overall[, 'SLOF']
 	tb = as.data.frame(cbind(diffscore, stats))
 	colnames(tb) = c('diff', 'R', 'Moran')
@@ -312,13 +316,15 @@ plotRMoransI <- function () {
 	tb[which(tb$diff > 0.2), 'diff'] = 0.2
 	tb[which(tb$diff < - 0.2), 'diff'] = -0.2
 
-	ggplot(tb, aes(x = Moran, y = R, color = diff)) + geom_point(cex = 3.5) + ylab(R_title) + xlab(morans_title) + 
+	p = ggplot(tb, aes(x = Moran, y = R, color = diff)) + geom_point(cex = 3.5) + ylab(R_title) + xlab(morans_title) + 
 	theme_classic() + theme(legend.margin=margin(0,0,0,10), legend.position="right", legend.box = "horizontal", axis.text=element_text(size=15), legend.key.size=unit(2, "line"), axis.title=element_text(size=19), legend.text=element_text(size=16), legend.title=element_text(size=19, margin = margin(l = -10, unit = "pt")) ) +
-	scale_colour_gradientn(values=c(0, 0.25, 0.35, 0.4999, 0.5001, 0.65, 0.75, 1), colours = (brewer.pal(name='RdBu', n = 11)[c(1, 2, 3, 8, 9, 10, 11)]),limit = limit, name = "    ROC AUC\n(DAO - SLOF)")
+	scale_colour_gradientn(values=c(0, 0.25, 0.35, 0.4999, 0.5001, 0.65, 0.75, 1), colours = (brewer.pal(name='RdBu', n = 11)[c(1, 2, 3, 8, 9, 10, 11)]),limit = limit, name = "   ROC AUC\n(DAO - SLOF)")
+	ggsave("slof.png", width=9)
+	print(p)
+
 	dev.off()
 
 
-	pdf("knn.pdf", width=9)
 	diffscore = overall[, 'DAO'] - overall[, 'KNN']
 	tb = as.data.frame(cbind(diffscore, stats))
 	colnames(tb) = c('diff', 'R', 'Moran')
@@ -326,13 +332,15 @@ plotRMoransI <- function () {
 	tb[which(tb$diff> 0.2), 'diff'] = 0.2
 	tb[which(tb$diff< - 0.2), 'diff'] = -0.2
 
-	ggplot(tb, aes(x = Moran, y = R, color = (diff))) + geom_point(cex = 3.5) + 
+	p = ggplot(tb, aes(x = Moran, y = R, color = (diff))) + geom_point(cex = 3.5) + 
 	ylab(R_title) + xlab(morans_title) + scale_colour_gradientn(values=c(0, 0.25, 0.35, 0.4999, 0.5001, 0.65, 0.75, 1), colours = (brewer.pal(name='RdBu', n = 11)[c(1, 2, 3, 8, 9, 10, 11)]),limit = limit, name = "   ROC AUC\n(DAO - KNN)") +
 	theme_classic() + theme(legend.margin=margin(0,0,0,10), legend.position="right", legend.box = "horizontal", axis.text=element_text(size=15), legend.key.size=unit(2, "line"), axis.title=element_text(size=19), legend.text=element_text(size=16), legend.title=element_text(size=19, margin = margin(l = -10, unit = "pt")) )
+	ggsave("knn.png", width=9)
+	print(p)
+
 	dev.off()
 
 
-	pdf("lof.pdf", width=9)
 	diffscore = overall[, 'DAO'] - overall[, 'LOF']
 	tb = as.data.frame(cbind(diffscore, stats))
 	colnames(tb) = c('diff', 'R', 'Moran')
@@ -340,13 +348,15 @@ plotRMoransI <- function () {
 	tb[which(tb$diff> 0.2), 'diff'] = 0.2
 	tb[which(tb$diff< - 0.2), 'diff'] = -0.2
 
-	ggplot(tb, aes(x = Moran, y = R, color = (diff))) + geom_point(cex = 3.5) + ylab(R_title) + xlab(morans_title) + 
+	p = ggplot(tb, aes(x = Moran, y = R, color = (diff))) + geom_point(cex = 3.5) + ylab(R_title) + xlab(morans_title) + 
 	scale_colour_gradientn(values=c(0, 0.25, 0.35, 0.4999, 0.5001, 0.65, 0.75, 1), colours = (brewer.pal(name='RdBu', n = 11)[c(1, 2, 3, 8, 9, 10, 11)]),limit = limit, name = "  ROC AUC\n(DAO - LOF)") +
 	theme_classic() + theme(legend.margin=margin(0,0,0,10), legend.position="right", legend.box = "horizontal", axis.text=element_text(size=15), legend.key.size=unit(2, "line"), axis.title=element_text(size=19), legend.text=element_text(size=16), legend.title=element_text(size=19, margin = margin(l = -10, unit = "pt")) )
+	ggsave("lof.png", width=9)
+	print(p)
+
 	dev.off()
 
 
-	pdf("oracle.pdf", width=9)
 	orc = c()
 	for(i in 1:nrow(overall)){
 		j = sort.list(t(overall[i, c(1:3)]), dec = T)[1]
@@ -365,9 +375,37 @@ plotRMoransI <- function () {
 	tb[which(tb$diff< - 0.2), 'diff'] = -0.2
 
 
-	ggplot(tb, aes(x = Moran, y = R, color = (diff), shape = orc$Algorithms)) + geom_point(cex = 3.5) + labs(shape="") + ylab(R_title) + xlab(morans_title) + 
+	p = ggplot(tb, aes(x = Moran, y = R, color = (diff), shape = orc$Algorithms)) + geom_point(cex = 3.5) + labs(shape="") + ylab(R_title) + xlab(morans_title) + 
 	scale_colour_gradientn(values=c(0, 0.25, 0.35, 0.4999, 0.5001, 0.65, 0.75, 1), colours = (brewer.pal(name='RdBu', n = 11)[c(1, 2, 3, 8, 9, 10, 11)]),limit = limit, name = "    ROC AUC\n(DAO - Oracle)") +
 	theme_classic() + theme(legend.margin=margin(0,0,0,10), legend.position="right", legend.justification = 'bottom', legend.box = "vertical", axis.text=element_text(size=15), legend.key.size=unit(2, "line"), axis.title=element_text(size=19), legend.text=element_text(size=16), legend.title=element_text(size=19, margin = margin(l = -10, unit = "pt")) )
+	ggsave("oracle.png", width=9)
+	print(p)
+	
 	dev.off()
 
+}
+
+
+args = commandArgs(trailingOnly=TRUE)
+if(as.character(args[1]) == "summaryRealDatasets"){
+	summaryRealDatasets()
+}
+if(as.character(args[1]) == "summaryResultsSyntheticDatasets"){
+	plotSynth()
+	summaryResultsSyntheticDatasets()
+}
+if(as.character(args[1]) == "lrSyntheticDatasets"){
+	lrSyntheticDatasets()
+}
+if(as.character(args[1]) == "printRuntime"){
+	printRuntime()
+}
+if(as.character(args[1]) == "lrRealDatasets"){
+	lrRealDatasets()
+}
+if(as.character(args[1]) == "plot_R_MoransI"){
+	plot_R_MoransI()
+}
+if(as.character(args[1]) == "plotCDRealDatasets"){
+	plotCDRealDatasets()
 }
